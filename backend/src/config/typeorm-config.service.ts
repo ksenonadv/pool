368321e5c82c1c26ae1ecd7b706986b0b138e716
ksenonadv <cristeastefan1999@gmail.com>
2025-05-22 00:15:38 +0300

    use ssl env

--- backend/src/config/typeorm-config.service.ts
+++ backend/src/config/typeorm-config.service.ts
@@ -34,6 +34,7 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
       username: this.configService.get('DB_USERNAME', 'postgres'),
       password: this.configService.get('DB_PASSWORD', 'postgres'),
       database: this.configService.get('DB_NAME', 'pool'),
+      ssl: this.configService.get('DB_SSL', false),
       entities: [User, Match, MatchPlayer, PlayerStats, Cue],
       synchronize: true,
       logging: false
